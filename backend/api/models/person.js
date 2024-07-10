7277f428a1608c04882b1dba42d074fe61ea5c11
abker ranim <ranim.abker@enicar.ucar.tn>
2024-07-10 19:14:31 +0100

    Create user models,routes,controllers

---
+++ backend/api/models/person.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const personSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+
+
+});
+
+module.exports = mongoose.model('Person', personSchema);
