7a76b467c36818384f1ab9aafb450dd6956b018e
abker ranim <ranim.abker@enicar.ucar.tn>
2024-07-12 18:12:04 +0100

    Create Comment model, routes and controllers

---
+++ backend/api/routes/comment.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const commentController = require("../controllers/comment");
+
+router.get("/", commentController.getAllComments);
+router.post("/", commentController.createComment);
+router.get('/:commentId', commentController.createComment);
+router.delete('/:commentId', commentController.deleteComment);
+
+module.exports = router;
