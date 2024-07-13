3431c634ef0247a5989365e9d6f5d3ccf25eb923
abker ranim <ranim.abker@enicar.ucar.tn>
2024-07-13 20:22:56 +0100

    Add relationship between User and Comment

--- backend/api/models/comment.js
+++ backend/api/models/comment.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 
 const commentSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  content: { type: String, required: true },
+  content: {
+    type: String,
+    required: true
+  },
+  commentOwner: {
+    type: ObjectId,
+    ref: 'user'
+  },
 });
 
 module.exports = mongoose.model("comment", commentSchema);
