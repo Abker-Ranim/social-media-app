233dba0c0d3fd44d02409eb4042b5197ff92f481
abker ranim <ranim.abker@enicar.ucar.tn>
2024-07-13 19:55:33 +0100

    Add relationship between User and Message

--- backend/api/models/message.js
+++ backend/api/models/message.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 
 const messageSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  content: { type: String, required: true },
+  content: {
+    type: String,
+    required: true
+  },
+  sender: {
+    type: ObjectId,
+    ref: 'user'
+  },
+  receiver: {
+    type: ObjectId,
+    ref: 'user'
+  },
 });
 
 module.exports = mongoose.model("message", messageSchema);
