57ba1e431606d227f6093867e67f39ab0fdaefea
Rami Jebali <jebalirami7@gmail.com>
2024-07-14 17:26:24 +0100

    Fix Like model

--- backend/api/models/like.js
+++ backend/api/models/like.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const likeSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  content: { type: String, required: true },
   post: { type: mongoose.Schema.Types.ObjectId, ref: "post", required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
 });
