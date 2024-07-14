696460ee88dfb4768d173034c15dd5984012e591
abker ranim <ranim.abker@enicar.ucar.tn>
2024-07-15 00:12:01 +0100

    Add JWT authentication middleware  to Message, Commnet, Like, Post and User

--- backend/api/routes/like.js
+++ backend/api/routes/like.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const checkAuth = require('../middleware/checkAuth');
 const likeController = require("../controllers/like");
 
 router.get("/", likeController.getAllLikes);
-router.post("/", likeController.createLike);
-router.delete("/", likeController.deletelike);
+router.post("/", checkAuth,likeController.createLike);
+router.delete("/",checkAuth, likeController.deletelike);
 
 module.exports = router;
--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const checkAuth = require('../middleware/checkAuth');
 const userController = require("../controllers/user");
 
-router.get("/", userController.getAllUsers);
-router.post("/signup", userController.signupUser);
-router.post("/login", userController.loginUser);
+router.get("/", checkAuth,userController.getAllUsers);
+router.post("/signup",checkAuth, userController.signupUser);
+router.post("/login", checkAuth,userController.loginUser);
 
 module.exports = router;
