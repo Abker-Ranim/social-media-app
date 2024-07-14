0269cc1ad33951ca7567f466f7f93f5aa405803f
abker ranim <ranim.abker@enicar.ucar.tn>
2024-07-14 02:22:13 +0100

    Add Validation to the user

--- backend/api/controllers/user.js
+++ backend/api/controllers/user.js
@@ -14,65 +14,43 @@ exports.getAllUsers = (req, res, next) => {
 };
 
 exports.signupUser = (req, res, next) => {
-  
-  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-
-  if (!passwordRegex.test(req.body.password)) {
-    return res.status(400).json({
-      message: "Password must contain at least 8 characters, including letters and numbers"
-    });
-  }
-
-  User.find({ email: req.body.email })
-    .exec()
-    .then(users => {
-      if (users.length >= 1) {
-        return res.status(409).json({
-          message: "Email exists"
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              firstName: req.body.firstName,
-              lastName: req.body.lastName,
-              email: req.body.email,
-              password: hash
-            });
-
-            user
-              .save()
-              .then(result => {
-                console.log(result);
-                res.status(201).json({
-                  message: "User created successfully",
-                  createdUser: {
-                    _id: result._id,
-                    firstName: result.firstName,
-                    lastName: result.lastName,
-                    email: result.email,
-                  },
-                });
-              })
-              .catch(err => {
-                res.status(500).json({
-                  error: err
-                });
-              });
-          }
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      return res.status(500).json({
         error: err
       });
+    }
+
+    const user = new User({
+      _id: new mongoose.Types.ObjectId(),
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hash
     });
+
+    user.save()
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+          message: "User created successfully",
+          createdUser: {
+            _id: result._id,
+            firstName: result.firstName,
+            lastName: result.lastName,
+            email: result.email,
+          },
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 };
+
+
+
 
 
--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -14,11 +14,17 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
     unique : true ,
-    match : /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    match : /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ ,
   },
   password: {
     type: String,
-    required: true
+    required: true ,
+    validate: {
+      validator: function(v) {
+        return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(v);
+      },
+      message: 'Password must contain at least 8 characters, including letters and numbers'
+    }
   },
 });
 
