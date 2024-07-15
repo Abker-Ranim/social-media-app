e26aa20a6a7770c77ccf95159a64e049b76db7a0
abker ranim <ranim.abker@enicar.ucar.tn>
2024-07-16 00:48:49 +0100

    Remove checkAuth mdidleware from user Authentication

--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const checkAuth = require('../middleware/checkAuth');
 const userController = require("../controllers/user");
 
-router.get("/", checkAuth,userController.getAllUsers);
-router.post("/signup",checkAuth, userController.signupUser);
-router.post("/login", checkAuth,userController.loginUser);
+router.get("/", checkAuth, userController.getAllUsers);
+router.post("/signup", userController.signupUser);
+router.post("/login", userController.loginUser);
 
 module.exports = router;
