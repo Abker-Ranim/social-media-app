445a96c55e96014e49f65610505fc260b3f3449c
abker ranim <ranim.abker@enicar.ucar.tn>
2024-07-17 16:07:41 +0100

    Add CRUD operations ( put and delete)

--- backend/api/controllers/post.js
+++ backend/api/controllers/post.js
@@ -17,7 +17,6 @@ exports.createPost = (req, res, next) => {
     _id: new mongoose.Types.ObjectId(),
     content: req.body.content,
     postOwner: req.body.postOwner,
-    postComment: req.body.postComment
   });
 
   post
@@ -30,7 +29,6 @@ exports.createPost = (req, res, next) => {
           _id: result._id,
           content: result.content,
           postOwner: result.postOwner,
-          postComment: req.body.postComment
         },
       });
     })
@@ -41,7 +39,7 @@ exports.createPost = (req, res, next) => {
 
 exports.getPostById = (req, res, next) => {
   const id = req.params.postId;
-  Post.findById(id).populate('postOwner').populate('postComment')
+  Post.findById(id).populate('postOwner')
     .exec()
     .then((post) => {
       if (post) {
@@ -52,5 +50,77 @@ exports.getPostById = (req, res, next) => {
     })
     .catch((err) => {
       res.status(500).json(err);
+    });
+};
+
+exports.updatePost = (req, res, next) => {
+  const id = req.params.postId;
+  const newContent = req.body.content;
+
+  Post.findById(id)
+    .exec()
+    .then((post) => {
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+
+      if (post.postOwner.toString() !== req.userData.userId) {
+        return res.status(403).json({ message: "You are not authorized to update this post" });
+      }
+
+
+      Post.findOneAndUpdate(
+        { _id: id },
+        { $set: { content: newContent } },
+        { new: true }
+      )
+        .then((updatedDocument) => {
+          res.status(200).json(updatedDocument);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({
+            error: 'An error occurred while updating the post',
+          });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: 'An error occurred while searching for the post',
+      });
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .exec()
+    .then((post) => {
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+
+
+      if (post.postOwner.toString() !== req.userData.userId) {
+        return res.status(403).json({ message: "You are not authorized to delete this post" });
+      }
+
+
+      Post.findByIdAndDelete(postId)
+        .then(() => {
+          res.status(200).json({ message: "Post deleted successfully" });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: 'An error occurred while deleting the post' });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: 'An error occurred while searching for the post',
+      });
     });
 };
