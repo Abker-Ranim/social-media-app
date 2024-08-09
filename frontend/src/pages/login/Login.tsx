7558a77cbad1ddc1c9c5ed740f1391a6433e8f0f
abker ranim <ranim.abker@enicar.ucar.tn>
2024-08-10 00:11:17 +0100

    modify design login page

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,34 +1,78 @@
-
+import { useState } from "react";
 import "./login.css";
-import { FaLock } from "react-icons/fa";
+import { FaLock, FaLockOpen } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
-
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [checked, setChecked] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [passwordIcon, setPasswordIcon] = useState(<FaLock />);
+
+    const handleIconClick = () => {
+        setShowPassword(!showPassword);
+        setPasswordIcon(showPassword ? <FaLock /> : <FaLockOpen />);
+    }
+
+    const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const user = {
+            email,
+            password,
+            remember: checked,
+        };
+        console.log(user);
+        
+        setErrorMsg("Invalid Email or Password"); 
+    }
+
     return (
-        <div className="wrapper" >
-            <form action="">
+        <div className="wrapper">
+            <form onSubmit={handleLogin}>
                 <h1>Login</h1>
                 <div className="input-box">
-                    <input type="text" placeholder="Email" required />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
                     <MdEmail className="icon" />
-
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder="Password" required />
-                    <FaLock className="icon" />
-
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <span onClick={handleIconClick} className="icon">
+                        {passwordIcon}
+                    </span>
                 </div>
                 <div className="remember-forgot">
-                    <label >  <input type="checkbox" />Remember me </label>
-                    <a href="#">Forgot password?</a>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={checked}
+                            onChange={(e) => setChecked(e.target.checked)}
+                        />
+                        Remember me
+                    </label>
+                    <a href="#" onClick={() => alert("Réinitialiser le mot de passe")}>Forgot password?</a>
                 </div>
+                {errorMsg && <div className="error">{errorMsg}</div>}
                 <button type="submit">Login</button>
                 <div className="register-link">
-                    <p>Don't have an account ?<a href="#">Register</a></p>
+                    <p>Don't have an account? <a href="#">Register</a></p>
                 </div>
             </form>
         </div>
-    )
+    );
 }
-export default Login
+
+export default Login;
