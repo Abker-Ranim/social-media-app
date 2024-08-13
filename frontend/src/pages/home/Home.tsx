4d6eebaddb4307170a239d72813c64e4144832a5
abker ranim <ranim.abker@enicar.ucar.tn>
2024-08-13 19:23:36 +0100

    Modify design Home page

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,20 +1,20 @@
 import Navbar from "../../components/navbar/Navbar";
 import Posts from "../../components/posts/Posts";
-
 import Sidebar from "../../components/sidebar/Sidebar";
-import UsernameCard from "../../components/usernameCard/UsernameCard";
-
+import "./home.css";
 
 const Home = () => {
     return (
         <>
-
-            < Navbar />
-            <div className="layout-app">
-             <div style={{width:"25%"}}><UsernameCard /> <Sidebar /></div>
-
-            <div style={{width:"50%"}}><Posts /></div>
-            <div style={{width:"25%"}}>right</div>
+            <div className="app">
+                <Navbar />  
+                <div className="main-content">  
+                    <Sidebar /> 
+                    <div className="posts">  
+                        <Posts />
+                    </div>
+                    <div className="right"></div> 
+                </div>
             </div>
         </>
     );
