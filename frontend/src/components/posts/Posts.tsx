442e717eace0771d97ac1b2602adb897532d40c3
abker ranim <ranim.abker@enicar.ucar.tn>
2024-08-13 19:23:53 +0100

    Modify design posts

--- frontend/src/components/posts/Posts.tsx
+++ frontend/src/components/posts/Posts.tsx
@@ -1,18 +1,36 @@
-
+import { FaPlus } from "react-icons/fa6";
 import Post from "./post/Post";
 import "./posts.css";
 
 const Posts = () => {
     return (
         <>
-            
-<div className="posts">
-    <Post/>
-    <Post/>
-    <Post/>
-    
 
-</div>
+            <div className="posts">
+                <div className="new_post">
+                    <div className="new_post_details">
+                        <div className="new_post_profile_image">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="new_post_textbox">
+                            <textarea placeholder="what's in your mind..?"></textarea>
+                        </div>
+                    </div>
+                    <div className="new_post_buttons">
+                        <button>
+                            <FaPlus />
+
+                        </button>
+                    </div>
+                </div>
+                <div className="post_list"> <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                </div>
+
+
+            </div>
 
 
         </>
