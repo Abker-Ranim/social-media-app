c42764e632cc5c4192ea84de33aa5188beff49e9
abker ranim <ranim.abker@enicar.ucar.tn>
2024-08-13 19:22:36 +0100

    Integrate UsernameCard into Sidebar with icons and styling

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -2,31 +2,44 @@ import "./sidebar.css";
 import { GoHome } from "react-icons/go";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlinePermMedia } from "react-icons/md";
-import { IoPeopleOutline } from "react-icons/io5";
-import { IoSettingsOutline } from "react-icons/io5";
+import { IoPeopleOutline, IoSettingsOutline } from "react-icons/io5";
 
-const sidebar = () => {
+const Sidebar = () => {
     return (
-        <>
-
-            <div className="sidebar">
-                <div className="sidebar_menu">
-                    <a className="active" ><GoHome  className="margin"/>
-                    home</a>
-                    <a><FaRegUser className="margin"/>
-                    Profile</a>
-                    <a ><IoPeopleOutline className="margin"/>
-                    People</a>
-                    <a ><MdOutlinePermMedia className="margin"/>
-                    photos</a>
-                    <a ><IoSettingsOutline className="margin"/>
-                    settings</a>
+        <div className="sidebar">
+            <div className="usernameCard">
+                <div className="username_image">
+                    <img src="https://images.pexels.com/photos/27525165/pexels-photo-27525165/free-photo-of-lumineux-leger-paysage-gens.jpeg" />
+                </div>
+                <div className="username_info">
+                    <h3 className="username">ranim abker</h3>
+                    <span>@ranim</span>
                 </div>
             </div>
-
-
-        </>
+            <div className="sidebar_menu">
+                <a className="active">
+                    <GoHome className="margin" />
+                    home
+                </a>
+                <a>
+                    <FaRegUser className="margin" />
+                    Profile
+                </a>
+                <a>
+                    <IoPeopleOutline className="margin" />
+                    People
+                </a>
+                <a>
+                    <MdOutlinePermMedia className="margin" />
+                    photos
+                </a>
+                <a>
+                    <IoSettingsOutline className="margin" />
+                    settings
+                </a>
+            </div>
+        </div>
     );
 }
 
-export default sidebar;
+export default Sidebar;
