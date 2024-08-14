fa598e72dc73b438991946fd6056e028597896b5
Mohamed Rami Jebali <mohamedrami.jebali@enicar.ucar.tn>
2024-08-14 08:58:13 +0100

    Fix navbar styles

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -2,32 +2,31 @@ import { FaPlus } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 import "./navbar.css";
 
-
-const Navbar = ()=> {
-    return(
-<>
-<div className="navbar">
-    <div className="navbar_left">
-        <b>NEXUM</b>
-    </div>
-    <div className="navbar_right">
-        <div className="navbar_profile_search">
-            <IoIosSearch  className="small gray"/>
-            <input type="text" placeholder="Search..." />
-
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar">
+        <div className="navbar_left">
+          <b>NEXUM</b>
         </div>
-        <button className="navbar_profile_button">
-        <FaPlus />
-        <span className="text">Create</span>
-</button>
-       <div className="navbar_profile_image">
-       <img src="https://images.pexels.com/photos/27525165/pexels-photo-27525165/free-photo-of-lumineux-leger-paysage-gens.jpeg"  />
-       </div>
 
+        <div className="navbar_right">
+          <div className="navbar_profile_search">
+            <IoIosSearch className="small gray" />
+            <input type="text" placeholder="Search..." />
+          </div>
 
-    </div>
-</div>
-</>
-    );
-}
+          <button className="navbar_profile_button">
+            <FaPlus />
+            <span className="text">Create</span>
+          </button>
+
+          <div className="navbar_profile_image">
+            <img src="https://images.pexels.com/photos/27525165/pexels-photo-27525165/free-photo-of-lumineux-leger-paysage-gens.jpeg" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
