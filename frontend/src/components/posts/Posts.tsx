77946514c80be39e5e1cc71590e3307d226cb32a
Mohamed Rami Jebali <mohamedrami.jebali@enicar.ucar.tn>
2024-08-14 09:40:00 +0100

    Fix posts styles

--- frontend/src/components/posts/Posts.tsx
+++ frontend/src/components/posts/Posts.tsx
@@ -3,38 +3,35 @@ import Post from "./post/Post";
 import "./posts.css";
 
 const Posts = () => {
-    return (
-        <>
+  return (
+    <div className="posts">
+      <div className="new_post">
+        <div className="user_details">
+          <img
+            src="https://images.pexels.com/photos/27525165/pexels-photo-27525165/free-photo-of-lumineux-leger-paysage-gens.jpeg"
+            alt=""
+          />
+          <h3>Ranim</h3>
+        </div>
 
-            <div className="posts">
-                <div className="new_post">
-                    <div className="new_post_details">
-                        <div className="new_post_profile_image">
-                            <img src="" alt="" />
-                        </div>
-                        <div className="new_post_textbox">
-                            <textarea placeholder="what's in your mind..?"></textarea>
-                        </div>
-                    </div>
-                    <div className="new_post_buttons">
-                        <button>
-                            <FaPlus />
+        <textarea
+          className="new_post_textbox"
+          placeholder="what's in your mind..?"
+        ></textarea>
 
-                        </button>
-                    </div>
-                </div>
-                <div className="post_list"> <Post />
-                    <Post />
-                    <Post />
-                    <Post />
-                </div>
+        <button>
+          <FaPlus />
+        </button>
+      </div>
 
-
-            </div>
-
-
-        </>
-    );
-}
+      <div className="post_list">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
+};
 
 export default Posts;
--- frontend/src/components/posts/post/Post.tsx
+++ frontend/src/components/posts/post/Post.tsx
@@ -1,27 +1,25 @@
 import "./post.css";
 
 const Post = () => {
-    return (
-        <div className="post">
-            <div className="post_user_details">
-                <div className="user_image">
-                    <img 
-                        src="https://images.pexels.com/photos/27525165/pexels-photo-27525165/free-photo-of-lumineux-leger-paysage-gens.jpeg" 
-                        alt="User" 
-                    />
-                </div>
-                <div className="user_name">
-                    <h5>Ranim</h5>
-                    <span>14 hours ago</span>
-                </div>
-            </div>
-            <div className="post_content_details">
-                <p>Hello, this is my first post</p>
-            </div>
-            <div className="post_buttons">
-            </div>
+  return (
+    <div className="post">
+      <div className="post_user_details">
+        <img
+          src="https://images.pexels.com/photos/27525165/pexels-photo-27525165/free-photo-of-lumineux-leger-paysage-gens.jpeg"
+          alt="User"
+        />
+
+        <div className="user_name">
+          <h4>Ranim</h4>
+          <span>14 hours ago</span>
         </div>
-    );
-}
+      </div>
+
+      <div className="post_content_details">
+        <p>Hello, this is my first post</p>
+      </div>
+    </div>
+  );
+};
 
 export default Post;
