b13d8b56aeca61ea6bb2a6872aad9ba94477772c
abker ranim <ranim.abker@enicar.ucar.tn>
2024-09-15 20:18:04 +0100

    Add User Profile

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,18 +1,28 @@
-import "./sidebar.css";
+import React, { useState } from "react";
 import { GoHome } from "react-icons/go";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlinePermMedia } from "react-icons/md";
 import { IoPeopleOutline, IoSettingsOutline } from "react-icons/io5";
+import { Link, useNavigate } from "react-router-dom";
+import "./sidebar.css";
 
-const Sidebar = () => {
+const Sidebar: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<string>("home"); 
+  const navigate = useNavigate();
+  const userId = "ranim";
+
+  const handleTabClick = (tab: string) => { 0
+    setActiveTab(tab);
+  };
+
   return (
     <div className="sidebar">
       <div className="usernameCard">
         <img
           className="username_image"
           src="https://images.pexels.com/photos/27525165/pexels-photo-27525165/free-photo-of-lumineux-leger-paysage-gens.jpeg"
+          alt="User"
         />
-
         <div className="username_info">
           <h3 className="username">ranim abker</h3>
           <span>@ranim</span>
@@ -20,25 +30,48 @@ const Sidebar = () => {
       </div>
 
       <div className="sidebar_menu">
-        <a className="active">
+        <Link
+          to="/"
+          className={activeTab === "home" ? "active" : ""}
+          onClick={() => handleTabClick("home")}
+        >
           <GoHome className="margin" />
-          home
-        </a>
-        <a>
+          Home
+        </Link>
+
+        <a
+          onClick={() => {
+            handleTabClick("profile");
+            navigate(`/profile/${userId}`);
+          }}
+          className={activeTab === "profile" ? "active" : ""}
+        >
           <FaRegUser className="margin" />
           Profile
         </a>
-        <a>
+
+        <a
+          onClick={() => handleTabClick("people")}
+          className={activeTab === "people" ? "active" : ""}
+        >
           <IoPeopleOutline className="margin" />
           People
         </a>
-        <a>
+
+        <a
+          onClick={() => handleTabClick("photos")}
+          className={activeTab === "photos" ? "active" : ""}
+        >
           <MdOutlinePermMedia className="margin" />
-          photos
+          Photos
         </a>
-        <a>
+
+        <a
+          onClick={() => handleTabClick("settings")}
+          className={activeTab === "settings" ? "active" : ""}
+        >
           <IoSettingsOutline className="margin" />
-          settings
+          Settings
         </a>
       </div>
     </div>
