5429707e890629db6c479c8ee84cbcb3d75e4711
abker ranim <ranim.abker@enicar.ucar.tn>
2024-10-15 23:46:29 +0100

    Make sidebar  responsive for mobile and tablet screens

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoHome } from "react-icons/go";
 import { FaCircle, FaRegUser } from "react-icons/fa";
-import { MdOutlinePermMedia } from "react-icons/md";
-import { IoPeopleOutline, IoSettingsOutline } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../../helpers/AuthProvider";
 import { baseURL } from "../../api/axios";
@@ -10,69 +9,59 @@ import "./sidebar.css";
 
 const Sidebar: React.FC = () => {
   const navigate = useNavigate();
-
   const { auth } = useAuth();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const handleProfileClick = () => {
     navigate(`/profile/${auth?._id}`);
   };
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="sidebar">
-      <div className="usernameCard" onClick={handleProfileClick}>
-        {auth && (
-          <div className="image_container">
-            <img src={baseURL + "/" + auth?.profilePicture} alt="Profile" />
-            <FaCircle className="online" />
+    <>
+      <button className="hamburger" onClick={toggleSidebar}>
+        <div />
+        <div />
+        <div />
+      </button>
+
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <div className="usernameCard" onClick={handleProfileClick}>
+          {auth && (
+            <div className="image_container">
+              <img src={baseURL + "/" + auth?.profilePicture} alt="Profile" />
+              <FaCircle className="online" />
+            </div>
+          )}
+          <div className="username_info">
+            <h3 className="username">
+              {auth?.firstName} {auth?.lastName}
+            </h3>
+            <span>{auth?.email}</span>
           </div>
-        )}
-        <div className="username_info">
-          <h3 className="username">
-            {auth?.firstName} {auth?.lastName}
-          </h3>
-          <span>{auth?.email}</span>
+        </div>
+
+        <div className="sidebar_menu">
+          <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+            <GoHome className="margin" />
+            Home
+          </NavLink>
+
+          <NavLink to={`/profile/${auth?._id}`} className={({ isActive }) => (isActive ? "active" : "")}>
+            <FaRegUser className="margin" />
+            Profile
+          </NavLink>
+
+          <NavLink to={`/about`} className={({ isActive }) => (isActive ? "active" : "")}>
+            <IoSettingsOutline className="margin" />
+            About
+          </NavLink>
         </div>
       </div>
-
-      <div className="sidebar_menu">
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? "active" : "")}
-        >
-          <GoHome className="margin" />
-          Home
-        </NavLink>
-
-        <NavLink
-          to={`/profile/${auth?._id}`}
-          className={({ isActive }) => (isActive ? "active" : "")}
-        >
-          <FaRegUser className="margin" />
-          Profile
-        </NavLink>
-        <NavLink
-          to={`/people`}
-          className={({ isActive }) => (isActive ? "active" : "")}
-        >
-          <IoPeopleOutline className="margin" />
-          People
-        </NavLink>
-        <NavLink
-          to={`/photos`}
-          className={({ isActive }) => (isActive ? "active" : "")}
-        >
-          <MdOutlinePermMedia className="margin" />
-          Photos
-        </NavLink>
-        <NavLink
-          to={`/settings`}
-          className={({ isActive }) => (isActive ? "active" : "")}
-        >
-          <IoSettingsOutline className="margin" />
-          Settings
-        </NavLink>
-      </div>
-    </div>
+    </>
   );
 };
 
